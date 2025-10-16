b0d639e3800a77f814b329c476e6099c6c797552
Thighs <thighs800@gmail.com>
2025-10-16 16:18:56 +0900

    main.pyの完成

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from diaries.DiarySample import DiarySample
 from diaries.IwasakiDiary import IwasakiDiary
 from diaries.MiriDiary import MiriDiary
+from diaries.tsukadaDiary import TsukadaDiary
+from diaries.rioDiary import RioDiary
 
 
 # ↓のリストには、メンバーの各日記が格納されます。
 diaries = [DiarySample(), 
            IwasakiDiary(),
-           MiriDiary(),]
+           MiriDiary(),
+           TsukadaDiary(),
+           RioDiary(), 
+           ]
 
 
 for d in diaries:
